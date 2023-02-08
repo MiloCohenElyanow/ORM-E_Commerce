4b5b0a9fe7f92354ff50f6fcd3a4ff09adf6a679
Milo C <75094742+whotf1@users.noreply.github.com>
2023-02-08 00:47:04 -0600

    product models made they should all be linked back and fourth

--- models/index.js
+++ models/index.js
@@ -5,11 +5,8 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 // Categories have many Products
-
 // Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
